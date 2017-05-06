0724d6e9a4ef88b62ccefa5b4b2ea6ad9850e122
Kizem <samyzboss@hotmail.fr>
2017-05-07 00:39:01 +0200

    Formulaire de connexion
    Fond du formulaire.
    Bouton Se connecter.

--- views/connexion.view.php
+++ views/connexion.view.php
@@ -9,16 +9,19 @@
 	<div id = "main">
 		<header><?php include_once('includes/header.php'); ?></header>
 		<div align="center">
-			<h2>Connexion</h2>
+			<h2 class="titreconnex">Connexion</h2>
 			<br><br><br>
 			<form method="POST" action="">
-				<input type="email" name="mailconnect" placeholder="Mail" value="
-				<?php if(isset($mailconnect)) echo $mailconnect;?>"/>
+			<div class="form-connex">
+			<br><br>
+				<input type="email" name="mailconnect" placeholder="Mail" value="<?php if(isset($mailconnect)) echo $mailconnect;?>"/>
 				<input type="password" name="passwordconnect" placeholder="Mot de passe" />
 				<br /><br />
 				<input type="checkbox" name="rememberme" id="remembercheckbox"><label for="remembercheckbox">Se souvenir de moi</label>
 				<br /><br />
-				<input type="submit" name="formconnect" value="se connecter" />
+			</div>
+			<br><br><br>
+				<input class="btnconx" type="submit" name="formconnect" value="Se connecter" />
 			</form>
 			<?php
 				if (isset($erreur)) {
@@ -28,7 +31,7 @@
 					echo $message;
 				}
 			?>
-			<p>Pas encore de compte ? <a href="inscription.php">Inscrivez vous</a>.</p>
+			<p>Pas encore de compte ? <a href="inscription.php">Inscrivez vous</a>!</p>
 		</div>	
 		<footer><?php include_once("includes/footer.php"); ?></footer>
 	</div>
