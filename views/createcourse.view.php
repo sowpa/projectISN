b8e780bde0a24fecfba9a9c3662bc6aefa143c23
Kizem <samyzboss@hotmail.fr>
2017-05-17 09:57:48 +0200

    Design inscription et création de cours
    Nouveau design sur ces deux pages

--- views/createcourse.view.php
+++ views/createcourse.view.php
@@ -12,17 +12,42 @@
 		<div align="center">
 			<h2 class="titrecrea">Création de cours</h2>
 			<br><br><br>
-			<form name="creation" method="POST" action="">
+			<form class="form-horizontal" name="creation" method="POST" action="">
+			
 			<div class="form-crea">
 			<br>
-				<label for="titre">Le titre de votre cours : </label>
-				<input type="text" name="titre" id="titre" placeholder="Exemple : Echec et math" /><br /><br />
-				<label for="ville">La ville ou vous donnerez votre cours : </label>
-				<input type="text" name="ville" id="ville" placeholder="Exemple : Paris" /><br /><br />
-				<label for="description">Une courte description de votre cours : </label>
-				<textarea rows="3" name="description" id="description" placeholder="Exemple : Apprenez les math avec un élève de terminal S !" cols="24"></textarea><br /><br />
-				<label for="description_long">Une longue description de votre cours : </label>
-				<textarea rows="5" name="description_long" id="description_long" cols="24"></textarea><br /><br />
+				<div class="form-group">
+				<label class="col-md-4 control-label" for="titre">Le titre de votre cours : </label>
+					<div class="col-md-4">
+						<input id="titre" name="titre" type="text" placeholder="Exemple : Échec et maths" class="form-controlinput-md">
+					</div>
+				</div><br/><br />
+
+
+				<div class="form-group">
+  				<label class="col-md-4 control-label" for="ville">La ville où vous donnerez votre cours</label> 
+					<div class="col-md-4">
+						<input id="ville" name="ville" type="text" placeholder="Exemple : Paris" class="form-controlinput-md">
+					</div>
+				</div><br /><br />
+
+				<div class="form-group">
+				<label class="col-md-4 control-label" for="description">Une courte description de votre cours : </label>
+					<div class="col-md-4">
+						<textarea rows="3" name="description" id="description" placeholder="Exemple : Apprenez les math avec un élève de terminal S !" cols="24" class="form-controlinput-md"></textarea>
+					</div>
+				</div>				
+
+				<br/><br/>
+
+				<div class="form-group">
+				<label class="col-md-4 control-label" for="description_long">Une longue description de votre cours : </label>
+				<br/>
+					<div class="col-md-4">
+						<textarea rows="10" name="description_long" id="description_long" cols="40" class="form-controlinput-md"></textarea><br/>
+					</div>
+				</div>
+				<br/><br/>
 				
 			</div>
 
