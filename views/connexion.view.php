2e8a20525a0d338d4c7c84686d7920a698054811
Kizem <samyzboss@hotmail.fr>
2017-05-24 09:30:30 +0200

    Design
    - Design du formulaire de connexion
    - Design de l'affichage d'un cours

--- views/connexion.view.php
+++ views/connexion.view.php
@@ -6,24 +6,47 @@
 	<?php include_once('includes/links.php'); ?>
 	<link rel="stylesheet" type="text/css" href="design/connexion.css">
 </head>
+
 <body>
 	<div id = "main">
 		<header><?php include_once('includes/header.php'); ?></header>
 		<div align="center">
+
 			<h2 class="titreconnex">Connexion</h2>
+
 			<br><br><br>
+
 			<form method="POST" action="">
-			<div class="form-connex">
 			<br><br>
-				<input type="email" name="mailconnect" placeholder="Mail" value="<?php if(isset($mailconnect)) echo $mailconnect;?>"/>
-				<input type="password" name="passwordconnect" placeholder="Mot de passe" />
-				<br /><br />
+
+				<div class="form-connex">
+
+          		<div class="form-group">           	
+            	<div class="col-md-4">
+            	<input id="mailconnect" type="email" name="mailconnect" placeholder="Votre mail..." class="form-controlinput-md" value="<?php if(isset($mailconnect)) echo $mailconnect;?>"/>
+            	</div>
+          		</div>
+
+          		<div class="form-group">
+            	<div class="col-md-4">
+            	<input id="passwordconnect" type="password" name="passwordconnect" placeholder="Votre mot de passe..." class="form-controlinput-md" />
+            	</div>
+         	 	</div>
+
+				
+
+				<br/><br/>
 				<input type="checkbox" name="rememberme" id="remembercheckbox"><label for="remembercheckbox">Se souvenir de moi</label>
 				<br /><br />
-			</div>
+				</div>
+			
+
 			<br><br><br>
 				<input class="btnconx" type="submit" name="formconnect" value="Se connecter" />
 			</form>
+
+
+
 			<?php
 				if (isset($erreur)) {
 					echo $erreur;
@@ -32,9 +55,13 @@
 					echo $message;
 				}
 			?>
+			<br /><br />
+
 			<p>Pas encore de compte ? <a href="inscription.php">Inscrivez vous</a>!</p>
+
 		</div>	
 		<footer><?php include_once("includes/footer.php"); ?></footer>
 	</div>
 </body>
+
 </html>
--- views/cours.view.php
+++ views/cours.view.php
@@ -4,18 +4,24 @@
 	<title>Projet ISN</title>
 	<meta charset="utf-8">
 	<?php include_once('includes/links.php'); ?>
+	<link rel="stylesheet" type="text/css" href="design/cours.css">
 </head>
+
 <body>
 	<div id = "main">
 		<header><?php include_once('includes/header.php'); ?></header>
 		<div align="center">
-			<h2><?= $data['titre'] ?></h2>
+			<h2 class="titrecours"><?= $data['titre'] ?></h2>
 			<br><br><br>
 			<div>
 				<p><?= nl2br($data['description_long']) ?></p>
 			</div>
-			<a href="inscriptioncours.php?id=<?= $data['id'] ?>">S'inscrire au cours</a>
-		<footer><?php include_once("includes/footer.php"); ?></footer>
+			<a class ="inscripcours"href="inscriptioncours.php?id=<?= $data['id'] ?>">S'inscrire au cours</a>
+		</div>
+	<footer><?php include_once("includes/footer.php"); ?></footer>
 	</div>
+
 </body>
+
+
 </html>
