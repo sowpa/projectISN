b265bf123f8b350c6f1ed02995f05f335e4cd43a
AznMaknae <billy.vang27@gmail.com>
2017-05-28 19:27:13 +0200

    footer css

--- views/footer.view.php
+++ views/footer.view.php
@@ -1,3 +1,5 @@
+<link rel="stylesheet" type="text/css" href="design/footer.css">
+
 <?php if($showcookie) { ?>
 <div class="cookie-alert">
    En poursuivant votre navigation sur ce site, vous acceptez l’utilisation de cookies.<br /><a href="includes/accept_cookie.php">OK</a>
