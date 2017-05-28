fdcb6fe212ff1d8c3e01c4ad21b3e55973175259
AznMaknae <billy.vang27@gmail.com>
2017-05-28 19:01:46 +0200

    footer

--- views/footer.view.php
+++ views/footer.view.php
@@ -3,34 +3,65 @@
    En poursuivant votre navigation sur ce site, vous acceptez l’utilisation de cookies.<br /><a href="includes/accept_cookie.php">OK</a>
 </div>
 <?php } ?>
-
-
-<div class = "plus" id = "block">
-<h3><mark>En savoir plus<mark/> </h3>
-<p> 
-<a href="apropos.php">A propos</a> 
-<br/>
-<a href="ccm.php">Comment ça marche ?</a>
-</p>
-</div>
-
-<img src="ressources/footer_gauche">
-
-<div class = "qui" id = "block">
-<h3><mark>Qui sommes-nous ?<mark/> </h3>
-<p>
-<a href="CV/CV Samy">Samy Zaouadi</a>
-Billy Vang 
-Gabriel Colson
-</p>
-</div>
-
-<img src="ressources/footer_droit">
-
-<div class="contact" id = "block">
-<h3><mark>Contact<mark/></h3>
-<p>
-Adresse pour courrier (?) <br/>
-Numéro de téléphone (?)
-</p>
-</div>
+<div class="align-center">
+		<div class="container">
+			<div class="row">
+			  <!-- footer column 1 start -->
+				<div class="col-md-3">
+				<!-- row start -->
+					<div class="row">
+						<div class="col-sm-9">
+							<div class = "plus" id = "block">
+								<h3><mark>En savoir plus<mark/> </h3>
+								<p> 
+								<a href="apropos.php">A propos</a> 
+								<br/>
+								<a href="ccm.php">Comment ça marche ?</a>
+								</p>
+							</div>
+						</div>
+					</div>
+					<!-- row end -->
+				</div>
+				<!-- footer column 1 end -->
+				<div class="col-md-2">
+    		    	<img src="ressources/footer_gauche">
+    		    </div>
+    		    <!-- footer column 2 start -->
+    		    <div class="col-md-3">
+    		     <!-- row start -->
+    		    	 <div class="row">
+    		    	 	<div class="col-sm-9">
+								
+								<div class = "qui" id = "block">
+								<h3><mark>Qui sommes-nous ?<mark/> </h3>
+								<p>
+								<a href="CV/CV Samy">Samy Zaouadi</a>
+								Billy Vang 
+								Gabriel Colson
+								</p>
+								</div>
+						</div>
+        			 </div>
+        			  <!-- row end -->
+       			</div>
+        <!-- footer column 2 end -->
+        		<div class="col-md-1">
+        			<img src="ressources/footer_droit">
+        		</div>
+        <!-- footer column 3 start -->
+        		<div class="col-md-3">
+        		 <!-- row start -->
+        		 	<div class="row">
+        		 		<div class="col-sm-9">
+							
+								<div class="contact" id = "block">
+								<h3><mark>Contact<mark/></h3>
+								<p>
+								Adresse pour courrier (?) <br/>
+								Numéro de téléphone (?)
+								</p>
+								</div>
+						</div>
+					</div>
+				</div>
