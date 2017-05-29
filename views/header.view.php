2993ac183e86bfb1954b98073a606c72fbbfa99a
AznMaknae <billy.vang27@gmail.com>
2017-05-30 00:49:37 +0200

    header menu droite

--- views/header.view.php
+++ views/header.view.php
@@ -3,30 +3,31 @@
 
 <nav class="navbar navbar-fixed-left" id="my-navbar">
 	<div class = "container">
-		<div class = "navbar-header">
-
-			<a href="index.php"><img src="ressources/logo.png"></a>
-
 			<div class="collapse navbar-collapse" id="navbar-collapse">
-				<?php if($connect){ ?>
-				<!-- si l'utilisateur est connecté -->
-				<ul class="nav navbar-nav">
-					<li><a href="createcourse.php">créer un cours</a></li>
-					<li><a href="mescours.php">mes cours</a></li>
-					<li><a href="profil.php?id=<?php echo $_SESSION['id'];?>">mon profil</a></li>
-					<li><a href="deconnexion.php">se déconnecter</a></li>
-				</ul>
-
-				<?php }	else{ ?>
-				<!-- si l'utilisateur n'est pas connecté -->
+				<a href="index.php" class="navbar-left"><img src="ressources/logo.png"></a>
+					<div class = "navbar-header">
+				
+					
+					<?php if($connect){ ?>
+					<!-- si l'utilisateur est connecté -->
 					<ul class="nav navbar-nav">
-						<li><a href="connexion.php">se connecter</a>
-						<li><a href="inscription.php">s'inscrire</a>
+						<li><a href="createcourse.php">créer un cours</a></li>
+						<li><a href="mescours.php">mes cours</a></li>
+						<li><a href="profil.php?id=<?php echo $_SESSION['id'];?>">mon profil</a></li>
+						<li><a href="deconnexion.php">se déconnecter</a></li>
 					</ul>
 
-				<?php } ?>
+					<?php }	else{ ?>
+					<!-- si l'utilisateur n'est pas connecté -->
 
+						<ul class="nav navbar-nav">
+							<li><a href="connexion.php">se connecter</a>
+							<li><a href="inscription.php">s'inscrire</a>
+						</ul>
+
+					<?php } ?>
+
+					</div>
 			</div>
-		
 	</div>
 </nav>
