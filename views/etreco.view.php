644211a3244a564c4536a376802ccc1a5260cfef
Kizem <samyzboss@hotmail.fr>
2017-06-01 23:25:46 +0200

    Début page etreco

---
+++ views/etreco.view.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Projet ISN</title> 
+	<meta charset="utf-8">
+	<?php include_once('includes/links.php'); ?>
+	<link rel="stylesheet" type="text/css" href="design/etreco.css">
+</head>
+
+<body>
+	<div id = "main">
+		<header><?php include_once('includes/header.php'); ?></header>
+
+
+		<footer><?php include_once("includes/footer.php"); ?></footer>
+	</div>
+
+</body>
+</html>
