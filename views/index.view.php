02bb47e5b54a5d6ebb68d727ce9bb453818deea7
AznMaknae <billy.vang27@gmail.com>
2017-06-06 00:07:57 +0200

    index box

--- views/index.view.php
+++ views/index.view.php
@@ -35,66 +35,8 @@
       <?php } ?>
       </div>
       
-  <div class="row">
-    <div class ="centered">
-      <div class="col-sm-11 col-xs-11">
-          <div class="boutonun btn-fixed">
-            <a href="allcourses.php">Trouver un cours</a></div>
-      
-          <div class="boutondeux">
-            <a href="createcourse.php">Créer un cours</a></div>
-      </div>
-    </div>
-  </div>
-</div>
-                    <div class="col-lg-6  text-center">
-                        <div class="box">
-                            <div class="box-content">
-                             <!--   <a href="cours.php?id=<?= $c['c_id']?>" >
-              <h6><?= $c['titre'] ?> - <?= $c['pseudo']?></h6>
-              <div><?= $c['description'] ?></div>
-            </a>-->
-                                <a href="cours.php?id=<?= $c['c_id']?>" class="btn btn-block ">Voir ce cours</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-6  text-center">
-                        <div class="box">
-                            <div class="box-content">
-                                <h1 class="tag-title">Nom du cours</h1>
-                                <hr>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi pharetra quam sollicitudin nibh aliquam finibus. Etiam efficitur felis vel imperdiet varius. Maecenas bibendum elementum molestie. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris cursus finibus semper. Fusce molestie tincidunt leo vel varius. Nam scelerisque nulla feugiat leo consequat, id dignissim sem tincidunt. Proin elit mauris, hendrerit in varius sed, facilisis sit amet neque.</p>
-                                <br />
-                                <a href="#" class="btn btn-block ">Voir ce cours</a>
-                            </div>
-                        </div>
-                    </div>
-                              
-      </div>
-      <div class="row">
-                    <div class="col-lg-6 text-center">
-                        <div class="box">
-                            <div class="box-content">
-                                <h1 class="tag-title">Nom du cours</h1>
-                                <hr>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi pharetra quam sollicitudin nibh aliquam finibus. Etiam efficitur felis vel imperdiet varius. Maecenas bibendum elementum molestie. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris cursus finibus semper. Fusce molestie tincidunt leo vel varius. Nam scelerisque nulla feugiat leo consequat, id dignissim sem tincidunt. Proin elit mauris, hendrerit in varius sed, facilisis sit amet neque.</p>
-                                <br />
-                                <a href="#" class="btn btn-block">Voir ce cours</a>
-                            </div>
-                        </div>
-                      </div>
-                        <div class="col-lg-6 text-center">
-                        <div class="box">
-                            <div class="box-content">
-                                <h1 class="tag-title">Nom du cours</h1>
-                                <hr>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi pharetra quam sollicitudin nibh aliquam finibus. Etiam efficitur felis vel imperdiet varius. Maecenas bibendum elementum molestie. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris cursus finibus semper. Fusce molestie tincidunt leo vel varius. Nam scelerisque nulla feugiat leo consequat, id dignissim sem tincidunt. Proin elit mauris, hendrerit in varius sed, facilisis sit amet neque.</p>
-                                <br />
-                                <a href="#" class="btn btn-block">Voir ce cours</a>
-                            </div>
-                        </div>
-                      </div>
-      </div>
+
+                   
 
       
 
